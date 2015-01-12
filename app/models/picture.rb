cd31e1f73e78cade168ec1a72f00b8630a6b7afe
Aritra <asbanday@uwaterloo.ca>
2015-01-12 17:15:15 -0500

    no random screenshots

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,9 +2,6 @@ class Picture < ActiveRecord::Base
 
 	belongs_to :post 
 
-	has_attached_file :image, 
-		:styles => {:post_image => "140x110>",:user_image => "60x60>"},
-		:path => ":rails_root/public/images/:id/:filename",
-    :url  => "/images/:id/:filename"
+	has_attached_file :image, :styles => {:post_image => "140x110>",:user_image => "60x60>"}
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
