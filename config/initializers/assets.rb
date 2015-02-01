af75a895b959ab4505afe17e12205bc8847cbd58
Edward Zhang <edwardforcpu@gmail.com>
2015-02-01 18:43:37 -0500

    ops need Rails.application

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,5 +3,5 @@
 Rails.application.config.assets.precompile += %w( homepage.css )
 Rails.application.config.assets.precompile += %w( homepage.js )
 
-Rails.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
