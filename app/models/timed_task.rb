c435bf083a4e5965e62e6e3266aede15789cc828
Edward Zhang <edwardforcpu@gmail.com>
2015-09-13 13:35:00 -0400

    updated deliver! with deliver_now!

--- app/models/timed_task.rb
+++ app/models/timed_task.rb
@@ -1,20 +1,20 @@
 class TimedTask < ActiveRecord::Base
 	has_many :email_notification_settings
-	
+
 	def self.timed_jobs(unit)
 		@tasks = TimedTask.all.where(measure_of_time: unit)
 		@tasks.each do |task|
 			@settings = task.email_notification_settings
 			@settings.each do |setting|
 				@type = setting.settings_for
-				@user = setting.user	
+				@user = setting.user
 				@activities = PublicActivity::Activity.where(recipient_id: @user.id)
 				@activities = @activities.where(trackable_type: @type)
 				@activities = @activities.where(collected: false)
 				if !(@activities.empty?)
-						
-					Notifier.notification_update(@user,@activities).deliver!
-				
+
+					Notifier.notification_update(@user,@activities).deliver_now!
+
 				end
 				@activities.each do |act|
 					act.collected = true
