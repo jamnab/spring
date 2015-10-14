e8d5dcf7523653650c90d4891bd21bbb4b70acb6
Edward Zhang <edwardforcpu@gmail.com>
2015-10-14 02:42:15 -0400

    misplaced colon

--- app/models/user_invite.rb
+++ app/models/user_invite.rb
@@ -2,7 +2,7 @@ class UserInvite < ActiveRecord::Base
   belongs_to :department_entry
   has_one :organization, through: :department_entry
 
-  after_create: invite_email
+  after_create :invite_email
 
   def invite_email
     UserMailer.invite_email(self.organization, self.email).deliver_now
