5c3c08e68f758ec564a0d4c4c645eb7fc22541d1
Edward Zhang <edwardforcpu@gmail.com>
2015-10-30 19:12:03 -0400

    mailer function set up

--- app/mailers/beta_sign_up_mailer.rb
+++ app/mailers/beta_sign_up_mailer.rb
@@ -1,2 +1,19 @@
 class BetaSignUpMailer < ApplicationMailer
+  default from: 'beta@golaunchboard.com'
+
+  def beta_mailing_list_to_clint(bsu)
+    @email = bsu.email
+    @name = "#{bsu.first_name} #{bsu.last_name}"
+    mail(subject: "#{@name} wish to be added to beta mailing list", to: "accounts@golaunchboard.com")
+  end
+
+  def approved(bsu)
+    @bsu = bsu
+    mail(subject: "You have been approved to sign up", to: @bsu.email)
+  end
+
+  def denied(bsu)
+    @bsu = bsu
+    mail(subject: "Your beta application was not approved", to: @bsu.email)
+  end
 end
