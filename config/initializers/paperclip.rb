08be1266b3c2ab0c1f92958aa615c1f0fa900007
Edward Zhang <edwardforcpu@gmail.com>
2016-04-17 17:47:10 -0400

    wrong storage acc name

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,7 @@
 Paperclip::Attachment.default_options[:path] = ":class/:attachment/:id/:style/:filename"
 Paperclip::Attachment.default_options[:use_timestamp] = false
 Paperclip::Attachment.default_options[:azure_credentials] = {
-    storage_account_name: 'portfolio10cdn',
+    storage_account_name: 'launchboardcdn',
     access_key:           'QJvSH/6eEEUXQ8vpdz4BkOdaqPlBOfEboSuHeQdq3jLeIXviBX6BLmDiwZRK0Re/5JGzp6kv0Hf/1yKztT3Gug==',
     container:            'paperclip'
 }
