7b29aa70f12f78483e5403fe50846c6b83b516c6
Edward Zhang <edwardforcpu@gmail.com>
2016-04-17 17:22:50 -0400

    typo

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,9 +4,9 @@ class Picture < ActiveRecord::Base
 	belongs_to :organization
 
 	has_attached_file :image,
-    :storage: :azure,
-		:styles => {:user_image => "60x60>",:organization_image => "x40",:post_image => "150x100!", :max_image => "x500"},
-    :url  => ":azure_path_url"
+    storage: :azure,
+		styles: {:user_image => "60x60>",:organization_image => "x40",:post_image => "150x100!", :max_image => "x500"},
+    url: ":azure_path_url"
 
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
