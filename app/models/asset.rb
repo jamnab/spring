d33e61b4d52953ff09c97c135a98c4b7e9a5714d
Cong Wang <findcongwang@gmail.com>
2016-08-17 23:16:42 -0400

    fixed proposal upload and display

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,5 +5,5 @@ class Asset < ActiveRecord::Base
     storage: :azure,
     url: ":azure_path_url"
 
-  # validates_attachment_content_type :document, :content_type => /\Adocument\/.*\Z/
+  validates_attachment_content_type :document, :content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.ms-excel image/vnd.adobe.photoshop text/plain)
 end
