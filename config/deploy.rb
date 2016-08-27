3cc3f951b1662239b02313c0ffb47e70f7d7e5b5
Edward Zhang <edwardforcpu@gmail.com>
2016-08-27 18:22:44 -0400

    testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :deploy_via, :remote_cache
 set :linked_dirs, fetch(:linked_dirs, []).push('public/system', 'log', 'public/images', 'public/uploads', 'vendor/bundle')
 set :linked_files, fetch(:linked_files, []).push( 'config/secret.yml')
 
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)})--#{fetch(:stage)}--puma.sock"
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{shared_path}/log/puma.error.log"
